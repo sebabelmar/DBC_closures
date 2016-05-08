5594fb894b6e6c255c745403e6f3b7fb0f65ea21
sebabelmar <seba.belmar@devbootcamp.com>
2016-05-08 16:40:41 -0700

    scope looking better

--- src/scope.js
+++ src/scope.js
@@ -1,18 +1,36 @@
 // Trick to enable/disable funcs
-// window.onload = scope;
+window.onload = scope;
 
 function scope() {
 
-  // scope 'a'
-  var outNumber = 2;
 
-  function addIt() {
-    // scope 'b'
-    parameterNumber = arguments[0];
-    return parameterNumber * outNumber;
-  }
 
-  (function(){console.log("STOP-1...")})()
+	
+
+
+
+	// scope 'a'
+	var outNumber = 2;
+
+	function addIt() {
+		// scope 'b'
+		parameterNumber = arguments[0];
+		return parameterNumber*outNumber;
+	}
+
+	(function() {console.log("STOP-1...")})()
+
+	// Executables
+	// addIt(5)
+	// arguments[0]
+
+
+
+
+
+
+
+
 }
 
 
@@ -24,12 +42,9 @@ function scope() {
 
 
 
-
 // We are in the global scope (window)
-function globalControl (){return bueno}
-bueno = "SI"
-
-
+function globalControl() {return bueno}
+bueno = "SI";
 
 
 // Executables
